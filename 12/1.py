f75cc3e56cc8c2083347985031a6cf603aa83596
leg7 <dev@leonardgomez.xyz>
2025-01-03 01:53:36 +0100

    day 12 part 1 simplify

--- 12/1.py
+++ 12/1.py
@@ -6,44 +6,36 @@ map = sys.stdin.read().split()
 map_width = len(map[0])
 map_height = len(map)
 
-
 neighbor_offsets = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
-def tuple_add(a, b):
-    return (a[0] + b[0], a[1] + b[1])
-
-def out_of_bounds(lc):
-    global map_height, map_width
-    return lc[0] < 0 or lc[0] >= map_height or lc[1] < 0 or lc[1] >= map_width
-
-# simple bfs
-regions = []
+# regions = []
 visited = defaultdict(bool)
 price = 0
 for l,line in enumerate(map):
     for c,_type in enumerate(line):
+        # For each plant do a bfs to find the area and perimeter of it's region if not done already
         if visited[(l,c)]:
             continue
-
-        queue = deque()
-        queue.appendleft((l,c))
         visited[(l,c)] = True
         region_points = defaultdict(bool)
         region_points[(l,c)] = True
         perimeter = 0
 
+        queue = deque()
+        queue.appendleft((l,c))
         while len(queue) != 0:
             current_point = queue.pop()
 
-            valid_neighbor_count = 0
             for offset in neighbor_offsets:
-                neighbor = tuple_add(current_point, offset)
+                neighbor = (current_point[0] + offset[0], current_point[1] + offset[1])
 
-                if out_of_bounds(neighbor) or map[neighbor[0]][neighbor[1]] != _type:
+                out_of_bounds = neighbor[0] < 0 or neighbor[0] >= map_height or neighbor[1] < 0 or neighbor[1] >= map_width
+                same_type = not out_of_bounds and map[neighbor[0]][neighbor[1]] == _type
+
+                if out_of_bounds or not same_type:
                     perimeter += 1
 
-                if not out_of_bounds(neighbor) and map[neighbor[0]][neighbor[1]] == _type and visited[neighbor] == False:
-                    valid_neighbor_count += 1
+                if not out_of_bounds and same_type and not visited[neighbor]:
                     visited[neighbor] = True
                     region_points[neighbor] = True
                     queue.appendleft(neighbor)
@@ -52,9 +44,7 @@ for l,line in enumerate(map):
         region_price = area * perimeter
         price += region_price
 
-        regions.append(region_points)
-
-
+        # regions.append(region_points) # not needed for part 1 it seems
 
 print(price)
 
